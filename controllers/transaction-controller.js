d4ecb13230d9ded938b946bea7eacb58cb01675f
ayahmeftah <ayahmeftah123123@gmail.com>
2025-08-16 21:25:53 +0300

    created get all transactions controller with route

--- controllers/transaction-controller.js
+++ controllers/transaction-controller.js
@@ -13,7 +13,24 @@ async function createTransaction(req,res) {
 }
 
 
+// get all transactions
+async function getAllTransactions(req,res) {
+    try {
+        const allTransactions = await Transaction.find()
+
+        if(allTransactions){
+            res.status(200).json(allTransactions)
+        }else{
+            res.sendStatus(204)
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error : error.message})
+    }
+}
+
 
 module.exports = {
-    createTransaction
+    createTransaction,
+    getAllTransactions
 }
--- routes/transactions-routes.js
+++ routes/transactions-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const transactionController = require('../controllers/transaction-controller')
 
 router.post('/', transactionController.createTransaction)
+router.get('/', transactionController.getAllTransactions)
 
 
 
