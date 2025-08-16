b1601f756c87098c985c382f384fa4395394adf8
ayahmeftah <ayahmeftah123123@gmail.com>
2025-08-16 21:30:19 +0300

    created get one transaction controller with route

--- controllers/transaction-controller.js
+++ controllers/transaction-controller.js
@@ -1,36 +1,53 @@
 const Transaction = require('../models/Transaction')
 
 // create transaction
-async function createTransaction(req,res) {
+async function createTransaction(req, res) {
     try {
         const newTransaction = await Transaction.create(req.body)
         res.status(201).json(newTransaction)
-        
+
     } catch (error) {
         console.log("Error creating transaction: ", error)
-        res.status(500).json({error : error.message})
+        res.status(500).json({ error: error.message })
     }
 }
 
 
 // get all transactions
-async function getAllTransactions(req,res) {
+async function getAllTransactions(req, res) {
     try {
         const allTransactions = await Transaction.find()
 
-        if(allTransactions){
+        if (allTransactions) {
             res.status(200).json(allTransactions)
-        }else{
+        } else {
             res.sendStatus(204)
         }
     } catch (error) {
         console.log(error)
-        res.status(500).json({error : error.message})
+        res.status(500).json({ error: error.message })
     }
 }
 
 
+// get one transaction
+async function getOneTransaction(req, res) {
+    try {
+        const oneTransaction = await Transaction.findById(req.params.id)
+
+        if (oneTransaction) {
+            res.status(200).json(oneTransaction)
+        } else {
+            res.sendStatus(204)
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: error.message })
+    }
+}
+
 module.exports = {
     createTransaction,
-    getAllTransactions
+    getAllTransactions,
+    getOneTransaction
 }
--- routes/transactions-routes.js
+++ routes/transactions-routes.js
@@ -3,7 +3,7 @@ const transactionController = require('../controllers/transaction-controller')
 
 router.post('/', transactionController.createTransaction)
 router.get('/', transactionController.getAllTransactions)
-
+router.get('/:id', transactionController.getOneTransaction)
 
 
 module.exports = router
