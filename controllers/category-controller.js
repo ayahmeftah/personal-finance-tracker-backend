1461f1cc18f68522a1afbd0192a131f81ca3a4de
MalakAlmajed15 <malakalmajed15@gmail.com>
2025-08-16 14:59:42 +0300

    creating getCategories function

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -11,18 +11,19 @@ const createCategory = async (req, res) => {
 
 const getCategories = async (req, res) => {
     try {
-        
+        const allCategories = await Category.find()
+        if (allCategories) {
+            res.status(200).json(allCategories)
+        } else {
+            res.status(204)
+        }
     } catch (error) {
-        
+        res.status(500).json({error: error.message})   
     }
 }
 
 const showCategory = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
+
 }
 
 const deleteCategory = async (req, res) => {
