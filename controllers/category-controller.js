c05d99593211e37eff6b9acb30f52a1baacab4ea
MalakAlmajed15 <malakalmajed15@gmail.com>
2025-08-16 15:10:59 +0300

    creating updateCategory function

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -41,7 +41,7 @@ const deleteCategory = async (req, res) => {
         if (category) {
             res.status(200).json(category)
         } else {
-            res.status(400)
+            res.status(404)
         }
     } catch (error) {
         res.status(500).json({error: error.message})
@@ -50,9 +50,14 @@ const deleteCategory = async (req, res) => {
 
 const updateCategory = async (req, res) => {
     try {
-        
+        const category = await Category.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        if (category) {
+            res.status(200).json(category)
+        } else {
+            res.status(404)
+        }
     } catch (error) {
-        
+        res.status(500).json({error: error.message})
     }
 }
 
